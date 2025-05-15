6c2e3f519ffbb2555e6abc0962b3e5b28b56e9bc
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-05-15 18:52:36 +0300

    using the init method and self keyword

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):  # Correct spelling and default value
+        self.name = name
+        self.breed = breed
+
+# Create dogs OUTSIDE the class
+dog1 = Dog("Fido", "Dalmatian")
+dog2 = Dog("Fido")  # No breed, should default to "Mutt"
+
+print(dog1.name)   # Fido
+print(dog1.breed)  # Dalmatian
+
+print(dog2.name)   # Fido
+print(dog2.breed)  # Mutt
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+person = Person("Guido")
+
+print(person.name)
+    
